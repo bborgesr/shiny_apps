50d538fa14badab10a1e8a04ac67c3a11796b83a
Barbara Borges Ribeiro <bborgesr@macalester.edu>
2015-01-28 22:56:20 -0600

    changed read.delim input

--- dataOp/helpers.R
+++ dataOp/helpers.R
@@ -327,8 +327,7 @@ CIAdata <- function (code=NULL) {
     if (code %in% CIA$Code) {
       sub <- subset(CIA, Code==code)
       url <- (paste0("https://www.cia.gov/library/publications/the-world-factbook/rankorder/rawdata_", code, ".txt"))
-      table <- read.delim(textConnection(getURL(url, ssl.verifypeer=FALSE )), 
-                          header=FALSE, stringsAsFactors=FALSE)
+      table <- read.delim(url, header=FALSE, stringsAsFactors=FALSE)
       table[,1]<-NULL
       names(table) <- c("country", sub[["Name"]])
       table[[2]] = as.numeric(gsub("[^.+[:digit:] ]", "", table[[2]]))
